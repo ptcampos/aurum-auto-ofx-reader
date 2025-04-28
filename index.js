be554fa4f0905740667cc8560fd43511def76141
Paulo Campos <paulo@thriade.com>
2025-04-28 10:35:49 -0300

    Retorna 5 dias nas buscas por extrato

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const URLAURUMsParaEnviar = [
     "https://puketcuritiba.sistemaaurum.com"
 ];
 
-const DIAS_PARA_REGREDIR = 1;
+const DIAS_PARA_REGREDIR = 5;
 
 // Função para ler os arquivos
 async function retornaOsArquivosOfxDeXDiasAtrasAteHoje(
