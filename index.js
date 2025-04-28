a48a4c778e1336483e5edc94bd89a4b553b8e2d5
Paulo Campos <paulo@thriade.com>
2025-04-28 10:41:25 -0300

    Retorna 1 dia

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const URLAURUMsParaEnviar = [
     "https://puketcuritiba.sistemaaurum.com"
 ];
 
-const DIAS_PARA_REGREDIR = 2;
+const DIAS_PARA_REGREDIR = 1;
 
 // Função para ler os arquivos
 async function retornaOsArquivosOfxDeXDiasAtrasAteHoje(
